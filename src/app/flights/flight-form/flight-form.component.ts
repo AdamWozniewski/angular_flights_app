27373225c1024dd85777786133360910971773b3
Adam Woźniewski <adam.wozniewski@intive.com>
2019-03-20 12:42:22 +0100

    komponent dodawania lotu

--- src/app/flights/flight-form/flight-form.component.ts
+++ src/app/flights/flight-form/flight-form.component.ts
@@ -1,4 +1,6 @@
 import { Component, OnInit } from '@angular/core';
+import { FormBuilder, FormGroup, FormArray } from '@angular/forms';
+import { Crew } from 'src/app/core/models/Flight';
 
 @Component({
   selector: 'app-flight-form',
@@ -6,10 +8,61 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./flight-form.component.styl']
 })
 export class FlightFormComponent implements OnInit {
-
-  constructor() { }
+  form: FormGroup;
+  jobs = [
+    {
+      label: 'Stwaredess',
+      value: 'stwaredess',
+    },
+    {
+      label: 'Starszy załogant',
+      value: 'senior_cabin_crew',
+    },
+    {
+      label: 'Pilot',
+      value: 'pilot',
+    },
+    {
+      label: 'Główny pilot',
+      value: 'co_pilot',
+    },
+    {
+      label: 'Mechanik pokładowy',
+      value: 'mechanic',
+    },
+  ];
+  constructor(
+    private formBuilder: FormBuilder,
+  ) { }
 
   ngOnInit() {
+    this.buildForm();
   }
-
+  buildCrewMember() {
+    return this.formBuilder.group({
+      name: '',
+      job: '',
+    });
+  }
+  get crew() {
+    return this.form.get('crew') as FormArray;
+  }
+  addCrewMember() {
+    this.crew.push(this.buildCrewMember());
+  }
+  removeCrewMember(i: number) {
+    this.crew.removeAt(i);
+  }
+  private buildForm() {
+    this.form = this.formBuilder.group({
+      origin: '',
+      destination: '',
+      departureTime: '',
+      returnTime: '',
+      code: '',
+      additionalInformation: '',
+      withSKPlanesDiscount: false,
+      crew: this.formBuilder.array([this.buildCrewMember()])
+    });
+  }
 }
--- src/app/flights/flights.module.ts
+++ src/app/flights/flights.module.ts
@@ -5,12 +5,16 @@ import { FlightCardComponent } from './flight-card/flight-card.component';
 import { MaterialModule } from '../material/material.module';
 import { NewFlightComponent } from './new-flight/new-flight.component';
 import { FlightFormComponent } from './flight-form/flight-form.component';
+import { ReactiveFormsModule, FormsModule } from '@angular/forms';
 
 @NgModule({
   declarations: [FlightsComponent, FlightCardComponent, NewFlightComponent, FlightFormComponent],
   imports: [
     CommonModule,
     MaterialModule,
+
+    FormsModule,
+    ReactiveFormsModule
   ],
   entryComponents: [ // komponenty dynamiczne
     NewFlightComponent, 
